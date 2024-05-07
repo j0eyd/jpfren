64c0a52a1eddb235415ef73840cf1e319ef43e19
joey <joeydavid99@gmail.com>
2024-05-07 13:50:47 +0200

    centering, adding logo title

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 const fetch = require('node-fetch');
-module.exports = translateText;
+const https = require('https');
 
-async function translateText(inputString) {
-  const res1 = await fetch("https://libretranslate.com/translate", {
-    method: "POST",
-    body: JSON.stringify({
-      q: inputString,
-      source: "auto",
-      target: "en",
-      format: "text",
-      api_key: ""
-    }),
-    headers: { "Content-Type": "application/json" }
-  });
-  result = await res1.json();
-  if (result["detectedLanguage"] != "en"){
-    return result["translatedText"];
-  }
-  else {
-    const res2 = await fetch("https://libretranslate.com/translate", {
+module.exports = {
+  fetchTranslation,
+  getSourceLang,
+  translateText,
+  mockFetch
+};
+
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
+
+async function fetchTranslation(inputText, targetLang) {
+  try {
+    const response = await fetch("https://libretranslate.com/translate", {
       method: "POST",
       body: JSON.stringify({
-        q: inputString,
+        q: inputText,
         source: "auto",
-        target: "jp",
+        target: targetLang,
         format: "text",
         api_key: ""
       }),
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
+      agent
     });
-    result = await res2.json();
-    return result["translatedText"];
+
+    return response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
   }
 }
 
+async function getSourceLang(inputText) {
+  const data = await fetchTranslation(inputText, "en");
+  return data.detectedLanguage;
+}
+
+async function translateText(inputText) {
+  const sourceLang = await getSourceLang(inputText);
+  if (sourceLang != "ja") {
+    const data = await fetchTranslation(inputText, "ja");
+    return data.translatedText;
+  } else {
+    const data = await fetchTranslation(inputText, "en");
+    return data.translatedText;
+  }
+}
+
+translateText("Hello, world!").then(console.log);
+
+async function mockFetch(inputString) {
+  document.getElementById("translatedText").innerText = inputString + "fetched mock";
+}
